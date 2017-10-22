0275c42121573ad237a3ef18d2cc3bdf2dfe6f13
fitblip <fitblip@gmail.com>
2017-10-21 19:08:42 -0700

    Add in working CLI

--- certstream/__init__.py
+++ certstream/__init__.py
@@ -1,3 +1,3 @@
 from .core import listen_for_events
 
-__version__ = "1.1"
+__version__ = "1.2"
--- certstream/cli.py
+++ certstream/cli.py
@@ -1,6 +1,59 @@
+import argparse
+import datetime
+import json
+import logging
+import sys
+import termcolor
+
+from signal import signal, SIGPIPE, SIG_DFL
+
+import certstream
+
+parser = argparse.ArgumentParser(description='Connect to the CertStream and process CTL list updates.')
+
+parser.add_argument('--json', action='store_true', help='Output raw JSON to the console.')
+parser.add_argument('--full', action='store_true', help='Output all SAN addresses as well')
+parser.add_argument('--disable-colors', action='store_true', help='Disable colors when writing a human readable ')
 
 def main():
-    print("Currently in development!")
+    args = parser.parse_args()
+
+    # Ignore broken pipes
+    signal(SIGPIPE, SIG_DFL)
+
+    def _handle_messages(message, context):
+        if args.json:
+            sys.stdout.flush()
+            sys.stdout.write(json.dumps(message) + "\n")
+            sys.stdout.flush()
+        else:
+            if args.disable_colors:
+                logging.debug("Starting normal output.")
+                payload = "{} {} - {} {}\n".format(
+                    "[{}]".format(datetime.datetime.fromtimestamp(message['data']['seen']).isoformat()),
+                    message['data']['source']['url'],
+                    message['data']['leaf_cert']['subject']['CN'],
+                    "[{}]".format(", ".join(message['data']['leaf_cert']['all_domains']))
+                )
+
+                sys.stdout.write(payload)
+            else:
+                logging.debug("Starting colored output.")
+                payload = "{} {} - {} {}\n".format(
+                    termcolor.colored("[{}]".format(datetime.datetime.fromtimestamp(message['data']['seen']).isoformat()), 'cyan', attrs=["bold", ]),
+                    termcolor.colored(message['data']['source']['url'], 'blue', attrs=["bold",]),
+                    termcolor.colored(message['data']['leaf_cert']['subject']['CN'], 'green', attrs=["bold",]),
+                    termcolor.colored("[", 'blue') + "{}".format(
+                        termcolor.colored(", ", 'blue').join(
+                            [termcolor.colored(x, 'white', attrs=["bold",]) for x in message['data']['leaf_cert']['all_domains']]
+                        )
+                    ) + termcolor.colored("]", 'blue'),
+                )
+                sys.stdout.write(payload)
+
+            sys.stdout.flush()
+
+    certstream.listen_for_events(_handle_messages, skip_heartbeats=True)
 
 if __name__ == "__main__":
     main()
